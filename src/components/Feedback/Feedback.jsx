9288ef9df54e4ec37e70f3bb2c3e9eaf184a9e5c
MaksymPryhorko <78167917+MaksymPryhorko@users.noreply.github.com>
2021-09-19 12:46:11 +0300

    Refactoring incrementCount.
    Заменил setGood(good + 1) на  setGood((prevState) => prevState + 1).

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -20,15 +20,15 @@ export default function Feedback() {
   const incrementCount = (name) => {
     switch (name) {
       case "good":
-        setGood(good + 1);
+        setGood((prevState) => prevState + 1);
         break;
 
       case "bad":
-        setBad(bad + 1);
+        setBad((prevState) => prevState + 1);
         break;
 
       case "neutral":
-        setNeutral(neutral + 1);
+        setNeutral((prevState) => prevState + 1);
         break;
 
       default:
